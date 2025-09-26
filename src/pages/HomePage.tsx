821b13d5168e384f078ac77cd83cb07edd0faffe
shenghaosang-ops <shenghaosang@gmail.com>
2025-09-26 11:08:59 +0800

    style: 调整首页标题文字大小和间距

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,34 +1,30 @@
-import React from 'react';
-import { Input, Card, Row, Col } from 'antd';
-import { SearchOutlined } from '@ant-design/icons';
-
-const { Search } = Input;
+import  React from 'react';
+import { Card, Row, Col } from 'antd';
 
 const HomePage: React.FC = () => {
   const services = [
     {
       title: '客户信息查询系统',
       description: '精准查询 | 客户信息一秒即达',
-      icon: '�',
+      icon: '',
+      subIcon: '📋',
       link: 'https://portalqas.cnoocshell.com/home'
     },
     {
       title: '化免通线上报价平台',
       description: '线上报价 | 化免通让交易更灵活',
-      icon: '�',
+      icon: '',
+      subIcon: '💰',
       link: 'https://portalqas.cnoocshell.com/home'
     },
     {
       title: 'LMS物流管理系统',
       description: '智慧物流 | LMS全程可视可控',
-      icon: '🚛',
+      icon: '',
+      subIcon: '🚛',
       link: 'https://portalqas.cnoocshell.com/home'
     }
   ];
-
-  const handleSearch = (value: string) => {
-    console.log('搜索:', value);
-  };
 
   return (
     <div>
@@ -43,17 +39,10 @@ const HomePage: React.FC = () => {
       >
         <h1>砥砺前行，携手共创辉煌！</h1>
         <h2>中海壳牌，您的首选合作伙伴</h2>
-        <Search
-          className="search-input"
-          placeholder="请输入产品或服务关键词"
-          enterButton={<SearchOutlined />}
-          size="large"
-          onSearch={handleSearch}
-        />
       </div>
 
-      <div style={{ padding: '40px 0' }}>
-        <Row gutter={24}>
+      <div style={{ padding: '60px 0' }}>
+        <Row gutter={32}>
           {services.map((service, index) => (
             <Col span={8} key={index}>
               <Card 
@@ -61,9 +50,10 @@ const HomePage: React.FC = () => {
                 title={service.title}
                 onClick={() => window.open(service.link, '_blank')}
               >
-                <p>{service.description}</p>
-                <div style={{ fontSize: '2em', textAlign: 'center' }}>
-                  {service.icon}
+                <div className="service-description">{service.description}</div>
+                <div className="service-icons">
+                  <span className="main-icon">{service.icon}</span>
+                  <span className="sub-icon">{service.subIcon}</span>
                 </div>
               </Card>
             </Col>
